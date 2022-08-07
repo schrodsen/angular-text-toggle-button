fe5af0f37083c7371960f8859b0d44bc0cb346a6
schrodsen <schrodsen@kabelmail.de>
2022-08-07 11:14:46 +0200

    4 files modified

--- src/app/image-slider/image-slider.component.ts
+++ src/app/image-slider/image-slider.component.ts
@@ -1,4 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+interface IconGroup {
+  new: string,
+  marked: string,
+  unmarked: string,
+}
+
+interface Dictionary<T> {
+  [Key: string]: T;
+}
+
+interface ImageData {
+  url: string,
+  title: string,
+  shortDescription: string,
+}
+
+const demo_images: ImageData[] = [
+  {
+    url: 'https://cdn.reisenaktuell.com/images/2309880_78d42e6d8d6c7a6688b6d35d45a4a4aa-957x550-nocrop.jpg',
+    title: 'A1',
+    shortDescription: 'B1',
+  },
+  {
+    url: 'https://cdn.reisenaktuell.com/images/2309880_78d42e6d8d6c7a6688b6d35d45a4a4aa-957x550-nocrop.jpg',
+    title: 'A2',
+    shortDescription: 'B2',
+  },
+  {
+    url: 'https://cdn.reisenaktuell.com/images/2309880_78d42e6d8d6c7a6688b6d35d45a4a4aa-957x550-nocrop.jpg',
+    title: 'A3',
+    shortDescription: 'B3',
+  },
+  {
+    url: 'https://cdn.reisenaktuell.com/images/2309880_78d42e6d8d6c7a6688b6d35d45a4a4aa-957x550-nocrop.jpg',
+    title: 'A4',
+    shortDescription: 'B4',
+  },
+]
 
 @Component({
   selector: 'image-slider',
@@ -7,4 +47,27 @@ import { Component, Input } from '@angular/core';
 })
 
 export class ImageSliderComponent  {
+  @Input() IconGroupName: string;
+
+  definedIconGroups: Dictionary<IconGroup> = {
+    "default" : {
+      new : "fa-circle",
+      marked: "fa-check",
+      unmarked: "fa-xmark"
+    },
+    "bookmark" : {
+      new: "fa-bookmark-slash",
+      marked: "fa-bookmark",
+      unmarked: "fa-bookmark-slash"
+    },
+  }
+
+  images: ImageData[];
+  obsImages: Observable<ImageData[]>;
+
+  constructor () {
+    this.images = demo_images;
+    this.obsImages = of(this.images);
+  }
+
 }
--- src/app/image-slider/state-icon.directive.ts
+++ src/app/image-slider/state-icon.directive.ts
@@ -8,9 +8,20 @@ export class StateIconDirective {
 
   constructor(private el: ElementRef) {}
 
-  @HostListener('click')
-  onClick() {
-    this.el.nativeElement.classList.remove("fa-xmark");
-    this.el.nativeElement.classList.add("fa-check");
+  @HostListener('click', ["$event"])
+  onClick(event: any) {
+    event.stopPropagation();
+
+    let icon: HTMLElement = this.el.nativeElement.getElementsByTagName('i')[0];
+    if (icon !== undefined) {
+      if (icon.classList.contains('fa-bookmark')) {
+        icon.classList.remove("fa-bookmark");
+        icon.classList.add("fa-bookmark-slash");
+      } else {
+        icon.classList.remove("fa-circle");
+        icon.classList.remove("fa-bookmark-slash");
+        icon.classList.add("fa-bookmark");
+      }
+    }
   }
 }
